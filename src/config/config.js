f50b33de3b3fa8642c966aebdb374a3cd84850ef
RedGhoul <avaneesab5@gmail.com>
2020-09-03 10:28:46 -0400

    Yolo

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 export default {
     notifyMSG: "Scroll Down Below To See some of the stuff I have been working on",
     notifyMSGTimeOut: 3000000,
-    baseURL: "https://personal-site-api.experimentsinthedeep.com",
+    baseURL: "https://web-api-personal-site.experimentsinthedeep3.com",
     picSumBaseURL: "https://picsum.photos/id/",
     twitterIcon: "https://img.icons8.com/ios-glyphs/50/ffffff/twitter.png",
     linkedinIcon: "https://img.icons8.com/ios/48/ffffff/linkedin-filled.png",
